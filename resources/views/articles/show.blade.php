df72576ee43002832e26bf9d5475c22ea8d4d2b7
long <394307916@qq.com>
2018-04-22 06:55:54 +0000

    ziti

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -10,15 +10,18 @@
     border: 1px solid #eeeeee;
   }
   .content-body{
-    padding: 22px;
-    font-weight: normal;
+    padding-left:22px;
+    padding-right:22px;
+    padding-top:15px;
+    padding-bottom:15px;
+    font-family: "Arial","Microsoft YaHei","黑体","宋体",sans-serif;
   }
   .content-body a{
     color: #d6514d;
     text-decoration: underline;
   }
   .content-body h1,h2,h3,h4,h5,h6{
-    font-weight:500;
+    font-family: "Arial","Microsoft YaHei","黑体","宋体",sans-serif;
   }
   footer{
     margin-top: 22px;
