f875168c90c086c4d187875bc3502676fc6bde37
long <394307916@qq.com>
2018-04-22 06:35:56 +0000

    static page

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -2,12 +2,210 @@
 
 @section('style')
 <style type="text/css">
+
+* {
+	-webkit-box-sizing: border-box;
+	box-sizing: border-box;
+}
+
 body{
 	background-color: #f0f2f5;
+}
+
+.box {
+	background-color: #fff;
+	padding: 10px;
+	margin: 0 0 20px 0;
+	-webkit-box-shadow: 0 0.2em 0 0 #ddd, 0 0 0 1px #ddd;
+	box-shadow: 0 0.2em 0 0 #ddd, 0 0 0 1px #ddd;
+}
+
+.padding-sm{
+	padding: 10px;
+}
+
+.media:first-child {
+	margin-top: 0;
+}
+
+.media, .media-body {
+	zoom: 1;
+	overflow: hidden;
+}
+
+.media-left, .media-right, .media-body {
+	display: table-cell;
+	vertical-align: top;
+}
+
+.media-left, .media > .pull-left {
+	padding-right: 10px;
+}
+
+.topic-author-box {
+    position: relative;
+}
+
+.text-center {
+    text-align: center;
+}
+
+.topic-author-box .list-inline {
+    margin-bottom: 10px;
+}
+.list-inline {
+    padding-left: 0;
+    list-style: none;
+    margin-left: -5px;
+}
+
+.topic-author-box .list-inline li {
+    padding-left: 5px;
+    padding-right: 5px;
+    border: 1px solid #ccc;
+    border-radius: 12px;
+    padding: 2px 6px;
+    margin-left: 6px;
+    font-size: 0.9em;
+    color: #606060;
+    margin-top: 8px;
+}
+
+.list-inline > li {
+    display: inline-block;
+    padding-left: 5px;
+    padding-right: 5px;
+}
+
+#innerBg{
+	background:#fff;
+	margin:0 auto;
+	border-radius:4px;
+	-moz-border-radius:4px;
+	-o-border-radius:4px;
+	-webkit-border-radius:4px;
+	padding:20px 25px 35px 25px;
+}
+
+#about{
+	font-size:16px;
+}
+
+#about hgroup, p{
+	padding:0;
+	margin:0;
+}
+
+#about h1{
+	font-family: "Signika";
+	font-weight:400;
+	font-size:22px;
+	color:#CD5353;
+	margin:0 0 10px 0;
+	padding:0;
+
+}
+
+
+#about h4{
+	font-size:16px;
+	color:#666;
+	font-weight:bold;
+
+}
+
+#about h6{
+	font-size:14px;
+	color:#333;
+	font-weight:bold;
+	font-style:italic;
+}
+
+#skills, #introduction, #experience{
+	padding-bottom:10px;
+	margin-bottom:10px;
+	border-bottom:1px dotted #e3e2e2;
+}
+
+li {list-style-type:none;}
+ul {padding: 0px}
+
+#introduction ul li {
+	background:#efefef;
+    border-radius: 4px;
+	-moz-border-radius:4px;
+	-o-border-radius:4px;
+	-webkit-border-radius:4px;
+    color: #666666;
+    font-size: 11px;
+  	margin-bottom: 10px;
+	margin-top: 9px;
+	padding: 6px 44px 5px 10px;
+	border-left:3px solid #cccccc;
+}
+
+#introduction ul li span{
+	font-weight:bold;
+}
+
+.clearfix:before,
+.clearfix:after{
+	content:"";
+	display:table;
+}
+
+.clearfix:after{
+	clear:both;
 }
 </style>
 @stop
 
 @section('content')
 
+<div class="container-fluid" id="container-content">
+	<div class="row">
+
+		<div class="col-md-3">
+			<div class="box">
+				<div class="padding-sm">
+					<div style>
+						<div class="media">
+							<div class="media-left">
+								<div class="image">
+									<img src="/images/cxy.jpg" style="border-radius: 50%;">
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<hr>
+				<div class="topic-author-box text-center">
+					<ul class="list-inline">
+						<li>
+							<a href="https://github.com/394307916" target="_blank">Github</a>
+						</li>
+						<li>
+							<a href="https://www.zhihu.com/people/bai-yun-jian-46/activities" target="_blank">知乎</a>
+						</li>
+						<li>
+							<a href="https://laravel-china.org/users/23607" target="_blank">Laravel</a>
+						</li>
+					</ul>
+				</div>
+				<hr>
+				<div class="box text-center" style="box-shadow: 0 0 0 0 #ddd, 0 0 0 1px #ddd;">
+					一个永远的菜鸟.
+				</div>
+			</div>
+		</div>
+		<div class="col-md-9">
+			<div class="box">
+				@include('static_pages.introduce')
+			</div>
+		</div>
+
+
+	</div>
+
+</div>
 @stop
---
+++ resources/views/static_pages/introduce.blade.php
@@ -0,0 +1,49 @@
+<div id="innerBg">
+	<article id="about" class="clearfix">
+		<section id="introduction" class="clearfix">
+			<h1>关于我</h1>
+			<p>很水很水，水得我不想介绍自己了。。</p>
+			<ul>
+				<li><span>Name</span> : 子龙</li>
+				<li><span>Date of birth</span> : 1997</li>
+				<li><span>Location</span> : 中国</li>
+			</ul>
+
+			<ul>
+				<li><span>Email</span> : yangzilong97@gmail.com</li>
+				<li><span>Phone</span> : 不告诉你 </li>
+				<li><span>Website</span> : www.yzloo.com</li>
+			</ul>
+		</section>
+		
+		<section id="experience">
+			<h1>个人经历</h1>
+			<hgroup>
+				<h4>WebDesigner – zilong </h4>
+				<h6>2015-2019</h6>
+			</hgroup>
+			<p>前端写起来真痛苦</p>
+			<br />
+			<hgroup>
+				<h4>WebDevelopper – zilong </h4>
+				<h6>2015-2019</h6>
+			</hgroup>
+			<p>没想到后台写起来也痛苦</p>
+		</section>
+ 
+<!-- 		<section id="education">
+			<h1>教育水平</h1>
+			<hgroup>
+				<h4>SEO consulting – zilong </h4>
+				<h6>2005-2012</h6>
+			</hgroup>
+			<p>Ut enim ad minim veniam, quis nostrud exerc. Irure dolor in reprehend incididunt labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+			<br />
+			<hgroup>
+				<h4>Art director – 子龙 </h4>
+				<h6>2005-2012</h6>
+			</hgroup>
+			<p>Ut enim ad minim veniam, quis nostrud exerc. Irure dolor in reprehend incididunt labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+		</section> -->
+	</article>
+</div>
